53096207ff8559da1a0db14985c7687f5f71a2ef
sahelyromero <sahromeroca@unal.edu.co>
2023-10-06 20:15:34 -0500

    Intento 1

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,63 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma {
+	
+	private String[] letras;
+	private String interpretacion;
+	
+	public Alfabeto(String origen,String[] letras, String interpretacion) {
+		
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+		
+	}
+	
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+
+	public String[] getLetras() {
+		return letras;
+	}
+
+	public void setLetras(String[] letras) {
+		this.letras = letras;
+	}
+
+	public int cantidadLetras() {
+		
+		return this.letras.length;
+			
+	}
+	
+	public String interpretacion() {
+		
+		return this.getInterpretacion();
+		
+	}
+	
+	public String toString() {
+		
+		String imp = null;
+		
+		for (int i = 0; i < this.letras.length; i++) {
+			
+			if (i < this.letras.length-1) {
+				
+				imp += i + ", ";
+			}
+			
+			else {
+				
+				imp += i;
+			}
+		}
+		return imp;
+	}
+
+}
---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,55 @@
+package comunicacion;
+
+public class Fabula extends Escrito{
+	
+	private String ensenanza;
+	private String interpretacion;
+	
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		
+		super(origen, titulo, autor, paginas);
+		
+		this.ensenanza = ensenanza;
+		this.interpretacion = interpretacion;
+		
+	}
+
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	public int palabrasTotales(int parametro) {
+		
+		return (this.getPaginas()*parametro*1);
+		
+	}
+	
+	public String interpretacion() {
+		
+		return this.interpretacion;
+		
+	}
+	
+	public String toString() {
+		
+		return (this.getOrigen() + "\n"
+				+ this.getTitulo() + "\n"
+				+ this.getAutor() + "\n"
+				+ this.getPaginas() + "\n"
+				+ this.ensenanza);
+		
+	}
+
+}
